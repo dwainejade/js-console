143b2e59ba0ceef0de0fedf6639e9d317d60899d
Dwaine Matthew <46469243+dwainejade@users.noreply.github.com>
2022-02-03 12:38:55 -0500

    styling changes redesigning app based on new wireframes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useRef, useEffect } from "react";
-import "./styles.css";
+import "./styles.scss";
 import "codemirror/keymap/sublime";
 import "codemirror/lib/codemirror.css";
 import "codemirror/theme/material.css";
@@ -40,18 +40,16 @@ export default function App() {
 
   useEffect(() => {
     listEnd.current.scrollIntoView({
-      behavior: "smooth",
-      block: "end",
-      inline: "nearest"
+      behavior: "smooth"
     });
   }, [output]);
 
   function handleOutput(code) {
     try {
       let result = JSON.stringify(eval(code));
-      if (result === undefined) {
-        result = "no return";
-      }
+      // if (result === undefined) {
+      //   result = "";
+      // }
       if (console.logs) {
         setOutput(output.concat(console.logs, result));
       } else {
@@ -76,23 +74,8 @@ export default function App() {
 
   return (
     <div className={`app fade-in  ${theme.body}`}>
-      <div className="container">
+      <div className="editor-container">
         <div className="editor">
-          <div className="editor-header fade-in">
-            <button className="btn run-btn" onClick={() => handleOutput(code)}>
-              <FaPlay size={16} /> Run
-            </button>
-            <button
-              className={`btn theme-btn ${theme.body}`}
-              onClick={() => handleTheme()}
-            >
-              {theme.editor === "default" ? (
-                <FaMoon className="theme-icon light" size={18} />
-              ) : (
-                <FaSun className="theme-icon dark" size={18} />
-              )}
-            </button>
-          </div>
           <ControlledEditor
             onBeforeChange={handleInput}
             value={String(code)}
@@ -106,14 +89,36 @@ export default function App() {
             }}
           />
         </div>
-        <button className="btn clear-btn" onClick={() => clearConsole()}>
-          <MdDoNotDisturbAlt size={20} /> Clear
-        </button>
-        <div className={`console ${theme.console}`}>
-          {output.map((l, key = l.id) => (
-            <div className="cnsl-line">{l}</div>
-          ))}
-          <div ref={listEnd}></div>
+        <div className="button-container">
+          <button
+            className={`btn theme-btn ${theme.body}`}
+            onClick={() => handleTheme()}
+          >
+            {theme.editor === "default" ? (
+              <FaMoon className="theme-icon light" size={30} />
+            ) : (
+              <FaSun className="theme-icon dark" size={30} />
+            )}
+          </button>
+          <button className="btn run-btn" onClick={() => handleOutput(code)}>
+            Run <FaPlay size={30} />
+          </button>
+        </div>
+      </div>
+
+      <div className="bottom-container">
+        <div className="console-wrapper">
+          <div className={`console ${theme.console}`}>
+            {output.map((l) => (
+              <div className="cnsl-line">{l}</div>
+            ))}
+            <div ref={listEnd}></div>
+          </div>
+        </div>
+        <div className="button-container">
+          <button className="btn clear-btn" onClick={() => clearConsole()}>
+            Clear <MdDoNotDisturbAlt size={40} />
+          </button>
         </div>
       </div>
     </div>
